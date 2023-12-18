154902302e17acf4caf24cd787c747a8835593b2
QuinTran <149452198+QuinTran@users.noreply.github.com>
2023-12-18 02:32:45 +0000

    fix es-lint error

--- lib/actions/answer.action.ts
+++ lib/actions/answer.action.ts
@@ -1,4 +1,4 @@
-'use server'
+"use server";
 
 import Answer from "@/database/answer.model";
 import { connectToDataBase } from "../mongoose";
@@ -7,27 +7,29 @@ import { revalidatePath } from "next/cache";
 import Question from "@/database/question.model";
 
 export async function CreateAnswer(params: CreateAnswerParams) {
-    connectToDataBase()
-    try {
-        //destructure params
-        const { content, author, question, path } = params
+  connectToDataBase();
+  try {
+    // Destructure params
+    const { content, author, question, path } = params;
 
-        // create answer
-        const newAnswer = new Answer({
-            content, author, question
-        })
+    // create answer
+    const newAnswer = new Answer({
+      content,
+      author,
+      question,
+    });
 
-        // Populate Answer to Question
-        await Question.findByIdAndUpdate(question, {
-            $push: { answers: newAnswer._id },
-        });
+    // Populate Answer to Question
+    await Question.findByIdAndUpdate(question, {
+      $push: { answers: newAnswer._id },
+    });
 
-        // TODO: add interaction...
+    // TODO: add interaction...
 
-        await newAnswer.save();
-        revalidatePath(path)
-    } catch (error) {
-        console.log(error)
-        throw error
-    }
+    await newAnswer.save();
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
