61916cd89b0473c83be89f78ba3bcd9dfc14090d
QuinTran <149452198+QuinTran@users.noreply.github.com>
2023-12-22 07:28:01 +0000

    fixed the create question action

---
+++ app/(root)/tags/[tagId]/page.tsx
@@ -0,0 +1,58 @@
+import NoResult from "@/components/shared/NoResult";
+import QuestionCard from "@/components/shared/cards/QuestionCard";
+import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
+import { getQuestionsByTagId } from "@/lib/actions/tag.action";
+import React from "react";
+
+const TagDetail = async ({ params }: any) => {
+  const { tagId } = params;
+  const result = await getQuestionsByTagId({ tagId });
+  console.log(result);
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">{result.tagTitle}</h1>
+
+      <div className="mt-11 w-full">
+        <LocalSearchbar
+          route="/"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search tag question..."
+          otherClasses="flex"
+        />
+      </div>
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {result.questions.length > 0 ? (
+          result.questions.map((question: any, index: number) => (
+            <QuestionCard
+              key={index}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <div className="flex-center text-dark400_light500 mt-10 w-full">
+            <NoResult
+              title="There&#39;s no question to show"
+              description="Be the first to break the silence! 🚀 Ask a Question and kickstart the
+            discussion. our query could be the next big thing others learn from. Get
+            involved! 💡"
+              link="ask-question"
+              linkTitle="Ask a Question"
+            />
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default TagDetail;
