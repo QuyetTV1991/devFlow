c7347f16e638b9ac564308e5837da00675aec91d
QuinTran <149452198+QuinTran@users.noreply.github.com>
2023-12-28 16:49:27 +0800

    update the attribute for QuestionTab and AnswerTab

--- components/shared/tabs/AnswerTab.tsx
+++ components/shared/tabs/AnswerTab.tsx
@@ -1,27 +1,31 @@
 import React from "react";
 import AnswerCard from "../cards/AnswerCard";
 import { getAnswersByUserId } from "@/lib/actions/user.action";
+import { SearchParamsProps } from "@/types";
 
-interface AnswerTabProps {
+interface AnswerTabProps extends SearchParamsProps {
   userId: string;
+  clerkId?: string;
 }
 
-const AnswerTab = async ({ userId }: AnswerTabProps) => {
-  const result = await getAnswersByUserId({ userId });
+const AnswerTab = async ({ userId, searchParams, clerkId }: AnswerTabProps) => {
+  const result = await getAnswersByUserId({ userId, page = 1 });
   const allAnswers = result.answers;
   return (
-    <div>
+    <>
       {allAnswers.length > 0 &&
         allAnswers.map((answer: any, index: number) => (
           <AnswerCard
             key={index}
+            _id={answer._id}
             question={answer.question}
             author={answer.author}
             upvotes={answer.upvotes}
             createdAt={answer.createdAt}
+            clerkId={clerkId}
           />
         ))}
-    </div>
+    </>
   );
 };
 
